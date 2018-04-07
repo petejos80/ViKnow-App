6ff55610a7632c70de3d5145ac557f7c2a1f8cde
petejos80 <petejos80@gmail.com>
2018-04-07 12:42:27 -0500

    Added autosuggest search

--- assets/javascript/viknow.js
+++ assets/javascript/viknow.js
@@ -3,10 +3,10 @@ $("document").ready(function() {
 var search = "merlot"; //$("#search").val().trim(); THIS IS WHAT THE VARIABLE WILL ACTUALLY BE
 var recipe = "yannick-amirault-bourgueil-la-coudraye-loire-2009"; //This will target the clicked button object
 
-var queryURL = "http://api.snooth.com/wines/?q=" + search + "&akey=5pgy0fabib7s89ky9l5fx24ha754svspwnata652tn7gdr71";
-var pairingURL = "http://api.snooth.com/wine/?food=1&id=" + recipe + "&akey=5pgy0fabib7s89ky9l5fx24ha754svspwnata652tn7gdr71";
+var queryURL = "http://api.snooth.com/wines/?q=k&akey=5pgy0fabib7s89ky9l5fx24ha754svspwnata652tn7gdr71&n=100";
+var pairingURL = "http://api.snooth.com/wine/?food=1&id=" + recipe + "&akey=5pgy0fabib7s89ky9l5fx24ha754svspwnata652tn7gdr71&n=100";
 
-// Search Call and Response
+// Search Call and Responses
 
 $.ajax({
     url: queryURL,
@@ -16,6 +16,7 @@ $.ajax({
     var jsonResponse = JSON.parse(response);
 
     console.log(JSON.parse(response))
+    console.log(response);
 
     for(i = 0; i < jsonResponse.wines.length; i++) {
 
@@ -72,12 +73,75 @@ $.ajax({
 
 })
 
+// TEST AREA 
+// =========================================================================================================
 
+// var queryURL = "http://api.snooth.com/wines/?q=" + search + "&akey=5pgy0fabib7s89ky9l5fx24ha754svspwnata652tn7gdr71&n=100";
+// Instantiate the Bloodhound suggestion engine
 
+var wine = new Bloodhound({
+    datumTokenizer: function (datum) {
+        return Bloodhound.tokenizers.whitespace(datum.value);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {
+        url: 'http://api.snooth.com/wines/?q=%QUERY&api_key=f22e6ce68f5e5002e71c20bcba477e7d',
+        filter: function (wine) {
+            // Map the remote source JSON array to a JavaScript object array
+            return $.map(wine.results, function (wines) {
+                console.log(wine.results);
+                return {
+                    value: wines.name
+                };
+            });
+        }
+    }
+});
 
+// Initialize the Bloodhound suggestion engine
+wine.initialize();
 
+// Instantiate the Typeahead UI
+$('.typeahead').typeahead(null, {
+    displayKey: 'value',
+    source: wine.ttAdapter()
+});
 
-
-
+// =========================================================================================================
 
 })
+
+
+
+
+
+// SAMPLE CODE
+
+// Instantiate the Bloodhound suggestion engine
+// var movies = new Bloodhound({
+//     datumTokenizer: function (datum) {
+//         return Bloodhound.tokenizers.whitespace(datum.value);
+//     },
+//     queryTokenizer: Bloodhound.tokenizers.whitespace,
+//     remote: {
+//         url: 'http://api.themoviedb.org/3/search/movie?query=%QUERY&api_key=f22e6ce68f5e5002e71c20bcba477e7d',
+//         filter: function (movies) {
+//             // Map the remote source JSON array to a JavaScript object array
+//             return $.map(movies.results, function (movie) {
+//                 console.log(movies.results);
+//                 return {
+//                     value: movie.original_title
+//                 };
+//             });
+//         }
+//     }
+// });
+
+// // Initialize the Bloodhound suggestion engine
+// movies.initialize();
+
+// // Instantiate the Typeahead UI
+// $('.typeahead').typeahead(null, {
+//     displayKey: 'value',
+//     source: movies.ttAdapter()
+// });
