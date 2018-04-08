ee550c8753c44e962299356e742b5ace0d3c11fd
petejos80 <petejos80@gmail.com>
2018-04-07 19:06:38 -0500

    Removed redundant code

--- assets/javascript/viknow.js
+++ assets/javascript/viknow.js
@@ -114,36 +114,3 @@ $('.typeahead').typeahead(null, {
 })
 
 
-
-
-
-// SAMPLE CODE
-
-// Instantiate the Bloodhound suggestion engine
-// var movies = new Bloodhound({
-//     datumTokenizer: function (datum) {
-//         return Bloodhound.tokenizers.whitespace(datum.value);
-//     },
-//     queryTokenizer: Bloodhound.tokenizers.whitespace,
-//     remote: {
-//         url: 'http://api.themoviedb.org/3/search/movie?query=%QUERY&api_key=f22e6ce68f5e5002e71c20bcba477e7d',
-//         filter: function (movies) {
-//             // Map the remote source JSON array to a JavaScript object array
-//             return $.map(movies.results, function (movie) {
-//                 console.log(movies.results);
-//                 return {
-//                     value: movie.original_title
-//                 };
-//             });
-//         }
-//     }
-// });
-
-// Initialize the Bloodhound suggestion engine
-// movies.initialize();
-
-// Instantiate the Typeahead UI
-// $('.typeahead').typeahead(null, {
-//     displayKey: 'value',
-//     source: movies.ttAdapter()
-// });
